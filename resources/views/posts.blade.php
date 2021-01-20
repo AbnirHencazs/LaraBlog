153854b257b0ef70e2e991a3adf315ff08112b0d
BrianSanchez@Accesa <briansanchezizq@gmail.com>
2021-01-20 11:30:15 -0600

    Agrego visualizacion para las imagenes y con embebido

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -6,14 +6,14 @@
         <div class="col-md-8">
             @foreach($posts as $post)
             <div class="card mb-4">
+                @if($post->image)
+                    <img src="{{ $post->get_image }}" alt="" height="300px" class="card-img-top">
+                @elseif($post->iframe)
+                    <div class="embed-responsive embed-responsive-16by9">
+                        {!! $post->iframe !!}
+                    </div>
+                @endif
                 <div class="card-body">
-                    @if($post->image)
-                        <img src="{{ $post->get_image }}" alt="" class="card-img-top">
-                    @elseif($post->iframe)
-                        <div class="embed-responsive embed-responsive-16by9">
-                            {!! $post->iframe !!}
-                        </div>
-                    @endif
                     <h5 class="card-title">{{ $post->title }}</h5>
                     <p class="card-text">
                         {{ $post->get_excerpt }}<!-- Tener en cuenta que get_excerpt sera un mutador -->
